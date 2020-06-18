731a63a0b643f6634290e8ebc63c3f0ad0d58f92
Beny Hasanudin <bhasanudin@gmail.com>
2020-06-19 02:21:44 +0700

    Belajar route GET dan POST di express js

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res, next)=>{
-  res.send('Hello World!')    
+app.get('/', (req, res)=>{
+   res.send('GET request to the homepage')
 })
-
+app.post('/', (req, res)=>{
+  res.send('POST request to the homepage')
+})
 app.listen(port, ()=> console.log(`Server listening on port: http://localhost:${port}`))
